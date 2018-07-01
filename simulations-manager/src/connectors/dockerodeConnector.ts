21cd4eb2719911212cdeeeabb8bf69da410179bb
Pavel Kharakh <pavelkh@gmail.com>
2018-07-01 14:19:32 +0300

    fixing some tslint errors

--- simulations-manager/src/connectors/dockerodeConnector.ts
+++ simulations-manager/src/connectors/dockerodeConnector.ts
@@ -6,10 +6,10 @@ class DockerodeExtension extends Dockerode {
     getContainerIdByName(name: string) {
 
         // filter by name
-        var opts = {
+        const opts = {
             "limit": 1,
             "filters": `{"name": ["${name}"]}`
-        }
+        };
 
         return new Promise<string>((resolve, reject) => {
             this.listContainers(opts, function (err, containers) {
@@ -18,9 +18,9 @@ class DockerodeExtension extends Dockerode {
                 } else {
                     resolve(containers && containers[0].Id);
                 }
-            }); 
-        })
+            });
+        });
     }
 }
 
-export const dockerode = new DockerodeExtension({ host: "http://109.253.112.244", port: 4243 });
+export const dockerode = new DockerodeExtension({ host: "http://172.17.0.1", port: 4243 });
