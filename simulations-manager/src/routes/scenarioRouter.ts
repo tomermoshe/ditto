9e33b06e0892b748df90310e96e23aec2e8a5743
Pavel Kharakh <pavelkh@gmail.com>
2018-07-01 14:16:53 +0300

    Changing let variables to be const, where possible

--- simulations-manager/src/routes/scenarioRouter.ts
+++ simulations-manager/src/routes/scenarioRouter.ts
@@ -1,19 +1,19 @@
 import { Router, Request, Response } from "express";
-import { Scenario, scenarioModel } from "../models/Scenario";
+import { Scenario, ScenarioModel } from "../models/Scenario";
 import { ScenarioExecutor } from "../controllers/scenarioExecutor";
 
 interface ScenarioPlayRequest {
-    name: string
+    name: string;
 }
 
 export class ScenarioRouter {
     static routes(): Router {
         return Router()
             .post("/scenario/play", async (req: Request, res: Response) => {
-                let playRequest: ScenarioPlayRequest = req.body;
+                const playRequest: ScenarioPlayRequest = req.body;
 
-                let scenario: Scenario = <Scenario>(await scenarioModel.findOne({ name: playRequest.name }));
-                let scenarioExecutor: ScenarioExecutor = new ScenarioExecutor(scenario);
+                const scenario: Scenario = <Scenario>(await ScenarioModel.findOne({ name: playRequest.name }));
+                const scenarioExecutor: ScenarioExecutor = new ScenarioExecutor(scenario);
                 await scenarioExecutor.executeScenario();
                 res.status(200).send(`Scenario ${scenario.name} executed sucessfully`);
             });
