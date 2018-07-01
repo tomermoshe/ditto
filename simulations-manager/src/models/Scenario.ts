06286e35d253b3a2f38fca97f3b7ce380a0dc392
Pavel Kharakh <pavelkh@gmail.com>
2018-07-01 14:22:47 +0300

    adding sub schemas  and removing _id from array elements

--- simulations-manager/src/models/Scenario.ts
+++ simulations-manager/src/models/Scenario.ts
@@ -1,10 +1,5 @@
-import { SimulatorId, SimulatorInstanceId } from "./Simulator";
+import { SimulatorId, SimulatorInstanceId, SimulatorIdSchema, SimulatorInstanceIdSchema } from "./Simulator";
 import mongoose from "mongoose";
-
-
-
-
-
 
 export interface ScenarioStep {
     simulatorName: string;
@@ -15,29 +10,39 @@ export interface Scenario {
     name: string;
     simulators: Array<SimulatorInstanceId>;
     steps: Array<ScenarioStep>;
-
 }
 
 export type ScenarioDocument = Scenario & mongoose.Document;
 
 
-const scenarioSchema = new mongoose.Schema({
-    name: String,
-    simulators: [{
-        name: String,
-        id: {
-            type: {
-                imageName: String,
-                version: String
-            }
-        }
-    }],
-    steps: {
-        type: {
-            simulatorName: String,
-            command: Object
-        }
+const ScenarioStepSchema = new mongoose.Schema({
+    simulatorName: {
+        type: String,
+        required: true
+    },
+    command: {
+        type: Object,
+        required: true
     }
+}, { _id: false });
+
+
+const ScenarioSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    simulators: [{
+        type: SimulatorInstanceIdSchema,
+        _id: false,
+        required: true
+    }],
+    steps: [{
+        type: ScenarioStepSchema,
+        _id: false,
+        required: true
+    }]
 });
 
-export const scenarioModel = mongoose.model<ScenarioDocument>("Scenario", scenarioSchema);
+export const ScenarioModel = mongoose.model<ScenarioDocument>("Scenario", ScenarioSchema);
