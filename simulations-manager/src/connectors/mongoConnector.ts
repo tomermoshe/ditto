2eb09709f3eb5e491b8daf9199c7dcb30d5e6939
Pavel Kharakh <pavelkh@gmail.com>
2018-07-17 15:22:33 +0300

    Fix - retry connection on first attempt now working

--- simulations-manager/src/connectors/mongoConnector.ts
+++ simulations-manager/src/connectors/mongoConnector.ts
@@ -1,28 +1,34 @@
-import mongoose, { MongooseDocumentOptionals, Mongoose } from "mongoose";
+import mongoose, { MongooseDocumentOptionals, Mongoose, mongo } from "mongoose";
 import { MongosOptions } from "mongodb";
 import { reject } from "bluebird";
 
 export class MongoConnector {
   uri: string;
+  private readonly TIMEOUT_BETWEEN_CONNECTION_ATTEMPTS = 4000;
+
+  private readonly CONNECTION_TIMEOUT = 30000;
+
   constructor(uri: string) {
     this.uri = uri;
   }
   connect() {
+
     if (mongoose.connection.readyState) {
       return Promise.resolve(mongoose);
     }
 
+
     const connectionPromise: Promise<Mongoose> = new Promise((resolve, reject) => {
-      const options: mongoose.ConnectionOptions = {};
-      options.server = {
-        auto_reconnect: true,
+      const options: mongoose.ConnectionOptions = {
+        autoReconnect: true,
         poolSize: 5,
-        socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 },
+        keepAlive: 1,
+        connectTimeoutMS: 30000,
         reconnectTries: 3000
       };
 
 
-      mongoose.connect(this.uri, options);
+      mongoose.connect(this.uri, options).catch(err => console.log(err));
 
       const db = mongoose.connection;
 
@@ -34,7 +40,7 @@ export class MongoConnector {
         if (!mongoose.connection.readyState) {
           reject("Timeout when connecting to mongoDB");
         }
-      }, 30000);
+      }, this.CONNECTION_TIMEOUT);
 
       db.on("connecting", function () {
         console.info("MONGODB " + " connecting.");
@@ -42,6 +48,7 @@ export class MongoConnector {
 
       db.on("error", function (err) {
         console.log("MONGODB ERROR ", err);
+        mongoose.disconnect();
       });
 
       db.on("close", function (err) {
@@ -67,7 +74,12 @@ export class MongoConnector {
 
       db.on("disconnected", function () {
         console.info("MONGODB " + " disconnected");
-      });
+        console.log(this.uri);
+        setTimeout(() => {
+          mongoose.connect(this.uri, options);
+        }, this.TIMEOUT_BETWEEN_CONNECTION_ATTEMPTS);
+
+      }.bind(this));
     });
 
     return connectionPromise;
