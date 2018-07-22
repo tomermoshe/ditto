045bc7e44ab1702eafa792951c0be4a24209620b
Pavel Kharakh <pavelkh@gmail.com>
2018-07-22 19:40:24 +0300

    changing the api to return array of simulatorIds instead of simulator names

--- simulations-manager/src/routes/simulatorsRouter.ts
+++ simulations-manager/src/routes/simulatorsRouter.ts
@@ -10,11 +10,10 @@ const simulatorsProjection = {
 export class SimulatorRouter {
     static routes(): Router {
         return Router()
-            .get("/simulators/names", async (req: Request, res: Response) => {
-                const simulatorNames = await SimulatorConfigModel.find({}).select("id.imageName");
-                const array = simulatorNames.map(element => element.id.imageName);
-                const set = new Set(array);
-                res.status(200).json([...set]);
+            .get("/simulators", async (req: Request, res: Response) => {
+                const simulatorNames = await SimulatorConfigModel.find({});
+                const array = simulatorNames.map(element => element.id);
+                res.status(200).json(array);
             });
     }
 }
