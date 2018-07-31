023e9abe34064102ef4a0645bbeace2b9d1b86a3
Pavel Kharakh <pavelkh@gmail.com>
2018-07-31 17:35:55 +0300

    changing the access control allow header

--- simulations-manager/src/index.ts
+++ simulations-manager/src/index.ts
@@ -30,17 +30,11 @@ import { SimulatorRouter } from "./routes/simulatorsRouter";
     app.use(function (req, res, next) {
 
         // Website you wish to allow to connect
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-        // Request methods you wish to allow
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+        res.setHeader("Access-Control-Allow-Origin", "*");
 
         // Request headers you wish to allow
         res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
 
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader("Access-Control-Allow-Credentials", "http://localhost:3000");
 
         // Pass to next layer of middleware
         next();
