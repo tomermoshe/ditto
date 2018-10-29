0d6f5bd24b7216e193bc1678e35c41ffc0f39c4a
Pavel Kharakh <pavelkh@gmail.com>
2018-10-29 18:02:38 +0200

    Adding basic layout

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -4,13 +4,16 @@ import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import "./index.css";
+import "antd/dist/antd.css";
 import rootReducer from "./reducers";
 import ReduxThunk from "redux-thunk";
 import EnvironmentForm from "./components/EnvironmentForm";
 import nullUnregisredFields from "./middleware/nullUnregisredFields";
-import clearNullValues  from "./utils/form/clearNullValues";
+import clearNullValues from "./utils/form/clearNullValues";
 import ScenarioForm from "./components/ScenarioForm";
 import SimulatorUploadForm from "./components/SimulatorUploadForm";
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 
 
@@ -18,9 +21,25 @@ const store = createStore(rootReducer, applyMiddleware(ReduxThunk, nullUnregisre
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* <EnvironmentForm  /> */}
-    <ScenarioForm />
-    {/* <SimulatorUploadForm /> */}
+    <div>
+
+      <Layout>
+        <Header>Header</Header>
+        <Layout>
+          <Sider>Sider</Sider>
+          <Content>
+            <EnvironmentForm />
+          </Content>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
+
+
+      {/* <ScenarioForm /> */}
+      {/* <SimulatorUploadForm /> */}
+
+    </div>
+
   </Provider>,
   document.getElementById("root") as HTMLElement
 );
